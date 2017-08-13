d13590756302bf547b4041a78b9184c32214870d
jiayuqicz <jiayuqicz@gmail.com>
2017-08-13 21:06:56 +0800

    增加天气界面的布局

--- mygaode/src/main/java/com/jiayuqicz/mygaode/WeatherFragment.java
+++ mygaode/src/main/java/com/jiayuqicz/mygaode/WeatherFragment.java
@@ -1,13 +1,18 @@
 package com.jiayuqicz.mygaode;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class WeatherFragment extends Fragment {
+
+    TextView wind = null;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,4 +21,8 @@ public class WeatherFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_weather, container, false);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
