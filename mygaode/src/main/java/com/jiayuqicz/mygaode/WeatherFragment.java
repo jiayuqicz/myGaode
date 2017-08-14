56d26c69c9350bf32055882973e0dff3d5a3d332
jiayuqicz <jiayuqicz@gmail.com>
2017-08-14 17:27:46 +0800

    部分天气代码的增加

--- mygaode/src/main/java/com/jiayuqicz/mygaode/WeatherFragment.java
+++ mygaode/src/main/java/com/jiayuqicz/mygaode/WeatherFragment.java
@@ -8,11 +8,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.weather.LocalDayWeatherForecast;
+import com.amap.api.services.weather.LocalWeatherForecast;
+import com.amap.api.services.weather.LocalWeatherForecastResult;
+import com.amap.api.services.weather.LocalWeatherLive;
+import com.amap.api.services.weather.LocalWeatherLiveResult;
+import com.amap.api.services.weather.WeatherSearch;
+import com.amap.api.services.weather.WeatherSearchQuery;
 
-public class WeatherFragment extends Fragment {
+import java.util.List;
 
-    TextView wind = null;
 
+public class WeatherFragment extends Fragment implements WeatherSearch.OnWeatherSearchListener {
+
+    private TextView wind = null;
+    private TextView weather = null;
+    private TextView humidity = null;
+    private TextView temperature = null;
+    private TextView weatherForcast = null;
+
+    private String cityName = "北京市";
+
+    private WeatherSearchQuery query = null;
+    private WeatherSearch weatherSearch = null;
+    private LocalWeatherForecast forcast = null;
+    private LocalWeatherLive live = null;
+
+    private List<LocalDayWeatherForecast> forcastList = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,5 +47,58 @@ public class WeatherFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initView();
+        searchForcastWeather();
+        serchLiveWeather();
+    }
+
+    public void initView() {
+        wind = getView().findViewById(R.id.wind_value);
+        weather = getView().findViewById(R.id.weather_value);
+        humidity = getView().findViewById(R.id.humidity_value);
+        temperature = getView().findViewById(R.id.temperature_value);
+        weatherForcast = getView().findViewById(R.id.weather_forcast_value);
+
+
+    }
+
+    public void searchForcastWeather() {
+
+        query = new WeatherSearchQuery(cityName,WeatherSearchQuery.WEATHER_TYPE_FORECAST);
+        weatherSearch = new WeatherSearch(getContext());
+        weatherSearch.setOnWeatherSearchListener(this);
+        weatherSearch.setQuery(query);
+        weatherSearch.searchWeatherAsyn();
+
+    }
+
+    public void serchLiveWeather() {
+        query = new WeatherSearchQuery(cityName,WeatherSearchQuery.WEATHER_TYPE_LIVE);
+        weatherSearch = new WeatherSearch(getContext());
+        weatherSearch.setOnWeatherSearchListener(this);
+        weatherSearch.setQuery(query);
+        weatherSearch.searchWeatherAsyn();
+    }
+
+
+    @Override
+    public void onWeatherLiveSearched(LocalWeatherLiveResult localWeatherLiveResult, int rcode) {
+
+    }
+
+    @Override
+    public void onWeatherForecastSearched(LocalWeatherForecastResult localWeatherForecastResult,
+                                          int rcode) {
+        if (rcode == AMapException.CODE_AMAP_SUCCESS) {
+            if(localWeatherForecastResult != null && localWeatherForecastResult.getForecastResult()
+                    != null && localWeatherForecastResult.getForecastResult().getWeatherForecast()
+                    != null && localWeatherForecastResult.getForecastResult().getWeatherForecast()
+                    .size()>0) {
+                forcast = localWeatherForecastResult.getForecastResult();
+                forcastList = forcast.getWeatherForecast();
+
+            }
+        }
+
     }
 }
