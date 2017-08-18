5527973bb8ba01609640f1f88b3049e4eb13e045
jiayuqicz <jiayuqicz@gmail.com>
2017-08-18 21:01:10 +0800

    测试

--- mygaode/src/main/java/com/jiayuqicz/mygaode/SearchFragment.java
+++ mygaode/src/main/java/com/jiayuqicz/mygaode/SearchFragment.java
@@ -15,6 +15,7 @@ public class SearchFragment extends Fragment {
 
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
