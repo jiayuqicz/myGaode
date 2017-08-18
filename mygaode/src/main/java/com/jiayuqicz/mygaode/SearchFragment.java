06973c10c30a2c0f0ddddf9a67757008ab964c7f
jiayuqicz <jiayuqicz@gmail.com>
2017-08-18 20:37:44 +0800

    test

--- mygaode/src/main/java/com/jiayuqicz/mygaode/SearchFragment.java
+++ mygaode/src/main/java/com/jiayuqicz/mygaode/SearchFragment.java
@@ -14,7 +14,6 @@ import android.view.ViewGroup;
 public class SearchFragment extends Fragment {
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
