6535a5b3836f4d0f0deffbc5edf82284e2615007
jiayuqicz <jiayuqicz@gmail.com>
2017-09-01 15:47:32 +0800

    为设置改变增加提示

--- mygaode/src/main/java/com/jiayuqicz/mygaode/setting/SettingFragment.java
+++ mygaode/src/main/java/com/jiayuqicz/mygaode/setting/SettingFragment.java
@@ -1,13 +1,16 @@
 package com.jiayuqicz.mygaode.setting;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 
 import com.jiayuqicz.mygaode.R;
+import com.jiayuqicz.mygaode.util.ToastUtil;
 
 
-public class SettingFragment extends PreferenceFragment {
+public class SettingFragment extends PreferenceFragment implements
+        SharedPreferences.OnSharedPreferenceChangeListener{
 
     public static SettingFragment newInstance() {
         return new SettingFragment();
@@ -20,7 +23,35 @@ public class SettingFragment extends PreferenceFragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences().
+                unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch (key) {
+            case "show_today":
+                ToastUtil.show(getActivity(), getString(R.string.toast_change));
+                break;
+            case "map_switch_gensture":
+                ToastUtil.show(getActivity(), getString(R.string.toast_change));
+                break;
+            case "offline_map_location":
+                ToastUtil.show(getActivity(), getString(R.string.toast_change));
+                break;
+        }
     }
 }
--- mygaode/src/main/java/com/jiayuqicz/mygaode/util/ToastUtil.java
+++ mygaode/src/main/java/com/jiayuqicz/mygaode/util/ToastUtil.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 public class ToastUtil {
 
     public static void show(Context context, String msg) {
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
 
 
