bc84c94eca8f861ef15b9d10512369c4120f74a2
jiayuqicz <jiayuqicz@gmail.com>
2017-09-04 17:17:54 +0800

    增加欢迎页面

---
+++ mygaode/src/main/java/com/jiayuqicz/mygaode/main/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.jiayuqicz.mygaode.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jiayuqicz.mygaode.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int DELAY_TIME = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, DELAY_TIME);
+
+    }
+}
